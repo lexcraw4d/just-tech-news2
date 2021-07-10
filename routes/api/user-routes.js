48e5a5945f50bc752189b4ce0b325b2653347f51
Lex Crawford <lexcraw4d@gmail.com>
2021-07-09 21:57:27 -0400

    redid user model and api routes/connection/server

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,36 +1,38 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+
 // GET /api/users
 router.get('/', (req, res) => {
-    // Access our User model and run .findAll() method)
-    User.findAll({
-        attributes: { exclude: ['password'] }
-      })
-        .then(dbUserData => res.json(dbUserData))
-        .catch(err => {
-          console.log(err);
-          res.status(500).json(err);
-        });
+    // Access our User model and run .findAll() method) --> same as SELECT * FROM Users
+    User.findAll()
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
 // GET /api/users/1
 router.get('/:id', (req, res) => {
     User.findOne({
-        attributes: { exclude: ['password'] },
-        where: {
-          id: req.params.id
+        //SELECT * FROM users WHERE id = 1
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
         }
+        res.json(dbUserData);
       })
-        .then(dbUserData => {
-          if (!dbUserData) {
-            res.status(404).json({ message: 'No user found with this id' });
-            return;
-          }
-          res.json(dbUserData);
-        })
-        .catch(err => {
-          console.log(err);
-          res.status(500).json(err);
-        });
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
 
 // POST /api/users
 router.post('/', (req, res) => {
@@ -46,11 +48,9 @@ router.post('/', (req, res) => {
         res.status(500).json(err);
       });
   });
-
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
-  
     // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
     User.update(req.body, {
       where: {
